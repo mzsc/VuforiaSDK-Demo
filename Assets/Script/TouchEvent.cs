cc1fc5282e77bc2c6a04c5ab359c2814696fc7a9
lizhen <1126299593@qq.com>
2016-08-27 18:35:23 +0800

    增加旋转效果

---
+++ Assets/Script/TouchEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Moved)) {
+            TouchRotation();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Home) || Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+
+    private void TouchRotation() {
+        Vector2 TouchDelta = Input.GetTouch(0).deltaPosition;
+        Vector3 LowRotation = this.transform.rotation.eulerAngles;
+        Quaternion NewRotation = new Quaternion();
+        NewRotation.eulerAngles = new Vector3(LowRotation.x - TouchDelta.y, LowRotation.y - TouchDelta.x, LowRotation.z);
+        this.transform.rotation = NewRotation;
+    }
+}
